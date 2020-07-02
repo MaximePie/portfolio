6218d7c19c1a728993125c8efc047ff7e1a78835
Chatmalow <maxime.pie.mail@gmail.com>
2020-07-02 19:02:45 +0200

    feat: Create title

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './scss/_scss.scss';
 
-function App() {
+function Home() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Home">
+      <h1 className="Home__title">Maxime Pie - <span className="Home__title-side-text">Créateur</span></h1>
     </div>
   );
 }
 
-export default App;
+export default Home;
