12c987937afed1a96bc7e4b1689455e742aadddd
Chatmalow <maxime.pie.mail@gmail.com>
2020-07-23 15:09:02 +0200

    feat: Display navigation bar as icons on small screens

--- src/Navigation.jsx
+++ src/Navigation.jsx
@@ -1,13 +1,9 @@
 import React from 'react';
 import {Link, useLocation} from "react-router-dom";
+import {viewportContext} from "./contexts/viewport";
 
 export default function Navigation(props) {
-
-  /**
-   * Savoir si le lien est sélectionné.
-   * Quand on clique, il faut dire au composant quel lien est sélectionné.
-   * La différence est l'url.
-   */
+  const isMobile = React.useContext(viewportContext);
 
   const { pathname } = useLocation();
 
@@ -20,14 +16,40 @@ export default function Navigation(props) {
         className={"Navigation__link " + (selectedPath === '/' ? "Navigation__link--selected" : "")}
         onClick={() => {setSelectedPath('/')}}
       >
-        Accueil
+        {!isMobile && ("Accueil")}
+        {isMobile && (
+          <i className="fas fa-home"/>
+        )}
       </Link>
       <Link
         to="/myProjects"
         className={"Navigation__link " + (selectedPath === '/myProjects' ? "Navigation__link--selected" : "")}
         onClick={() => {setSelectedPath('/myProjects')}}
       >
-        Mes projets
+        {!isMobile && "Mes projets"}
+        {isMobile && (
+          <i className="fas fa-folder"/>
+        )}
+      </Link>
+      <Link
+        to="/aboutMe"
+        className={"Navigation__link " + (selectedPath === '/aboutMe' ? "Navigation__link--selected" : "")}
+        onClick={() => {setSelectedPath('/aboutMe')}}
+      >
+        {!isMobile && "Qui suis-je ?"}
+        {isMobile && (
+          <i className="fas fa-user"/>
+        )}
+      </Link>
+      <Link
+        to="/contact"
+        className={"Navigation__link " + (selectedPath === '/contact' ? "Navigation__link--selected" : "")}
+        onClick={() => {setSelectedPath('/contact')}}
+      >
+        {!isMobile && "Contact"}
+        {isMobile && (
+          <i className="fas fa-pen"/>
+        )}
       </Link>
     </div>
   );
